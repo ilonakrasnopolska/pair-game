4ece185ee645fba0678f194319f11a82484e7985
Ilona Krasnopolski <ilonakrasnopolskaya8@gmail.com>
2023-12-04 15:49:11 +0200

    created cards , stylized everything , add some pics

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+//array of picture
+const images = [
+  './img/dog1.png',
+  './img/dog2.png',
+  './img/dog3.png',
+  './img/dog4.png',
+  './img/dog5.png',
+  './img/dog6.png',
+  './img/dog7.png',
+  './img/dog8.png',
+  './img/dog9.png',
+]
 
 //function create div 
 function createDiv(className) {
@@ -26,18 +38,20 @@ function createParagraph(className, text) {
 function createList(className) {
   let ul = document.createElement('ul')
   ul.classList.add(className)
+  ul.classList.add('row')
   return ul
 }
-
 
 // Этап 1. Создайте функцию, генерирующую массив парных чисел. Пример массива, который должна возвратить функция: [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8].count - количество пар.
 
 function createNumbersArray(count) {
+  const numbers = Array.from({ length: 9 }, (_, index) => index + 1); // Создаем массив чисел от 1 до 9
+
   const pairedNumbers = [] //array of paired numbers
 
   // Generate paired numbers
   for (let i = 0; i < count / 2; i++) {
-    const randomNumber = Math.floor(Math.random() * 49) + 1 // Генерируем случайное число (для примера от 0 до 99)
+    const randomNumber = numbers.splice(Math.floor(Math.random() * numbers.length), 1)[0]; // Генерируем случайное число (для примера от 0 до 99)
     pairedNumbers.push(randomNumber, randomNumber); // Добавляем пару чисел в массив
   }
 
@@ -45,12 +59,9 @@ function createNumbersArray(count) {
 
 }
 
-const pairedNumbersArray = createNumbersArray(16); // Генерируем пары чисел
-console.log(pairedNumbersArray); // Выводим массив с парами чисел
-
 // Этап 2. Создайте функцию перемешивания массива.Функция принимает в аргументе исходный массив и возвращает перемешанный массив. arr - массив чисел
 
-function shuffle(arr) {
+function shuffleNumbers(arr) {
   let currentIndex = arr.length //current index 
 
   while (currentIndex !== 0) {
@@ -63,11 +74,55 @@ function shuffle(arr) {
   return arr
 }
 
-const shuffleArray = shuffle(pairedNumbersArray)
-console.log(shuffleArray);
-
 // Этап 3. Используйте две созданные функции для создания массива перемешанными номерами. На основе этого массива вы можете создать DOM-элементы карточек. У каждой карточки будет свой номер из массива произвольных чисел. Вы также можете создать для этого специальную функцию. count - количество пар.
 
-function startGame(count) {
+function createNewANumbersArr(count) {
+  const shuffleNumbersArr = createNumbersArray(count);
+  const numbersWithImages = shuffleNumbersArr.map(number => {
+    return {
+      number: number,
+      image: images[number - 1] // Назначаем изображение по соответствующему числу (-1, так как индексация начинается с 0)
+    };
+  });
 
+  return shuffleNumbers(numbersWithImages); // Перемешиваем массив с числами и изображениями
 }
+
+//Func create dom-element to card of game 
+function createCardOfGame(text,img) {
+  const card = document.createElement('li') //create li
+  card.classList.add('card')
+  card.classList.add('col-3')
+
+  card.addEventListener('click', () => { 
+   card.classList.toggle('card-active')
+  })
+
+  let image = document.createElement('img'); // create img
+  image.classList.add('card__image');
+  image.src = img; // src to img
+  
+  let number = createParagraph('card__number', text) //create p 
+
+  card.append(image, number) //add p to li
+
+  return card
+}
+
+//Func to start game
+function startGame(arr) {
+  for(i = 0; i < arr.length; i++) {
+    let createCard = createCardOfGame(arr[i].number, arr[i].image)
+    cardBox.append(createCard)
+  }
+}
+
+
+const container = createDiv('container') //container
+const title = createTitle('title', 'Pair Game') //title
+const cardBox = createList('card-box') //ul element for cards
+
+startGame(createNewANumbersArr(16)) //call func start game
+
+container.append(title, cardBox) //add title to container
+document.body.append(container) //add container to body
